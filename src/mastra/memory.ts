b9bccabb47c5151e353ef8f28ecb017238e811be
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-09-16 10:49:01 -0700

    add vector

--- src/mastra/memory.ts
+++ src/mastra/memory.ts
@@ -1,5 +1,6 @@
 import { Memory } from "@mastra/memory";
 import { fastembed } from "@mastra/fastembed";
+import { PgVector, PostgresStore } from "@mastra/pg";
 
 export const agentMemory = new Memory({
   embedder: fastembed,
@@ -10,6 +11,12 @@ export const agentMemory = new Memory({
       generateTitle: true,
     },
   },
+  storage: new PostgresStore({
+    connectionString: process.env.DATABASE_URL!,
+  }),
+  vector: new PgVector({
+    connectionString: process.env.DATABASE_URL!,
+  }),
 });
 
 // export const agentMemory: Memory = new Memory({
